c78065f93f16dd8fb56e0f6dd2f22adf6f4287f5
chiefexb <chief_exb@mail.ru>
2024-03-04 16:44:31 +0300

    123

--- debezium/con1.sh
+++ debezium/con1.sh
@@ -1,3 +1,4 @@
 curl -i -X POST -H "Accept:application/json" \
-  -H  "Content-Type:application/json" \ http://172.17.0.1:8083/connectors/ \
+  -H  "Content-Type:application/json" \
+  http://172.17.0.1:8083/connectors/ \
   -d @pg.json
