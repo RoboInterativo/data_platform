e7ff024c35d817a37d31533ea7112bf750de2ad3
chiefexb <chief_exb@mail.ru>
2024-03-04 15:24:18 +0300

    123

--- debezium/run.sh
+++ debezium/run.sh
@@ -3,4 +3,4 @@ docker run \
   -e GROUP_ID=1 \
   -e CONFIG_STORAGE_TOPIC=my_connect_configs \
   -e OFFSET_STORAGE_TOPIC=my_connect_offsets \
-  -e STATUS_STORAGE_TOPIC=my_connect_statuses  debezium/connect:1.2
+  -e STATUS_STORAGE_TOPIC=my_connect_statuses  -itd debezium/connect:1.2
