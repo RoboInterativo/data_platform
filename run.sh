d6d2c3eb2d42daff420f864c4725ff583c587264
chiefexb <chief_exb@mail.ru>
2024-03-04 14:00:13 +0300

    123

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-sudo docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=<YourStrong@Passw0rd>" \
+sudo docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD="$MSSQL_SA_PASSWORD \
    -p 1433:1433 --name sql1 --hostname sql1 \
    -d \
    mcr.microsoft.com/mssql/server:2022-latest
