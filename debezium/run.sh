37d591c6df41b90dc8c0648df65441c8ed190c7e
chiefexb <chief_exb@mail.ru>
2024-03-14 17:56:08 +0300

    123

--- debezium/run.sh
+++ debezium/run.sh
@@ -6,6 +6,6 @@ docker   run -p 8083:8083 \
   -e CONNECT_SASL_JAAS_CONFIG='org.apache.kafka.common.security.plain.PlainLoginModule required username=\"admin\" password=\"admin-secret\";' \
   -e CONFIG_STORAGE_TOPIC=my_connect_configs \
   -e OFFSET_STORAGE_TOPIC=my_connect_offsets \
-  -e STATUS_STORAGE_TOPIC=my_connect_statuses  -itd deb
+  -e STATUS_STORAGE_TOPIC=my_connect_statuses  -itd debezium/connect:1.9
 #  --add-host t-gp-cdc-db-1.dellin.local:10.214.66.156 \
   # -e KAFKA_OPTS="-Djava.security.auth.login.config=path/to/jaas.conf" \
