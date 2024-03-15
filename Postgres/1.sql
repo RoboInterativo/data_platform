8860e2aeddf93d59311e91dd21fa4d9fb1705d8b
chiefexb <chief_exb@mail.ru>
2024-03-15 14:17:01 +0300

    123

--- Postgres/1.sql
+++ Postgres/1.sql
@@ -1,14 +1,13 @@
-CREATE TABLE accounts (
+CREATE TABLE accounts2 (
   user_id SERIAL PRIMARY KEY,
   username VARCHAR (50) UNIQUE NOT NULL,
   password VARCHAR (50) NOT NULL,
   email VARCHAR (255) UNIQUE NOT NULL,
-  created_at TIMESTAMP NOT NULL,
-  last_login TIMESTAMP
+
 );
 
-INSERT INTO accounts(user_id,username,password,created_at,last_login)  VALUES
- (accounts(1,'username','password',NULL,NULL);
+INSERT INTO accounts2(user_id,username,password)
+ VALUES (1,'username','password');
 
 
  CREATE PUBLICATION dbz_publication
