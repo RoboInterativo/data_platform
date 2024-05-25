7c3782601629875cb237e827a926149a4f07e2c2
chiefexb <chief_exb@mail.ru>
2024-05-25 10:39:22 +0300

    123

--- airflow/scripts/rest.sh
+++ airflow/scripts/rest.sh
@@ -1,8 +1,8 @@
 export PATH=$PATH:/opt/mssql-tools/bin
 sqlcmd -S localhost -U SA -P Password! \
--Q """RESTORE DATABASE dbname
+-Q """RESTORE DATABASE AdventureWorksDW2022
 FROM DISK = '/data/AdventureWorksDW2022.bak'
-WITH MOVE 'Dbname_Empty' TO '/data/dbname.mdf',
-MOVE 'Dbname_Empty_log' TO '//data/dbname.ldf'
+WITH MOVE 'AdventureWorksDW2022' TO '/data/AdventureWorksDW2022.mdf',
+MOVE 'AdventureWorksDW2022_log' TO '/data/AdventureWorksDW2022.ldf'
 """
 #'/data/AdventureWorksDW2022.bak' WITH FILE = 1, NOUNLOAD, REPLACE, NORECOVERY, STATS = 5"
