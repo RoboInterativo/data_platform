4a8833f6d2dd8467da93e402c54f04b7bcd8f531
chiefexb <chief_exb@mail.ru>
2024-05-28 00:22:11 +0300

    123

--- airflow/conn.sh
+++ airflow/conn.sh
@@ -1,5 +1,2 @@
 airflow connections add 'my_prod_db'     \
---conn-json '{"conn_type": "odbc", "host": "sqlserver", "port": 1433, "extra": {"DRIVER": "ODBC Driver 17 for SQL Server", "SERVER": "t-estaff-dl.dellin.local", "DATABASE": "estaff_cut", "Trusted_Connection": "yes"}}'
-
-airflow connections add 'my_prod_db'     \
---conn-json '{"conn_type": "odbc", "host": "sqlserver", "port": 1433, "extra": {"DRIVER": "ODBC Driver 17 for SQL Server", "SERVER": "t-estaff-dl.dellin.local", "DATABASE": "test", "Trusted_Connection": "yes"}}'
+--conn-json '{"conn_type": "odbc", "host": "sqlserver", "port": 1433, "login": "SA",password": "exb021025!","schema": "AdventureWorksDW2022","extra": {"DRIVER": "ODBC Driver 17 for SQL Server", "SERVER": "sqlserver", "DATABASE": "AdventureWorksDW2022"}}'
