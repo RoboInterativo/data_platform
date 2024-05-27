04b6f3b6b0086a72e3f1844bd2fb664fa9082c8d
chiefexb <chief_exb@mail.ru>
2024-05-27 19:27:03 +0300

    123

--- airflow/scripts/rest.sh
+++ airflow/scripts/rest.sh
@@ -6,3 +6,5 @@ WITH MOVE 'AdventureWorksDW2022' TO '/data/AdventureWorksDW2022.mdf',
 MOVE 'AdventureWorksDW2022_log' TO '/data/AdventureWorksDW2022.ldf'
 """
 #'/data/AdventureWorksDW2022.bak' WITH FILE = 1, NOUNLOAD, REPLACE, NORECOVERY, STATS = 5"
+
+#!
