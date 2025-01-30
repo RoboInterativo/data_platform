3df68e5f265cfc99f9dec17856fea5b47dc1c318
chiefexb <chief_exb@mail.ru>
2025-01-30 18:49:09 +0300

    123

--- kafka4/certs/cert3.sh
+++ kafka4/certs/cert3.sh
@@ -7,4 +7,5 @@ openssl req -new -key client-key.pem -out client.csr \
 -subj "/C=RU/ST=Moscow/L=Moscow/O=MyCompany/CN=kafka-client.mycompany.com"
 
 # Подписание CSR с использованием корневого сертификата
-openssl x509 -req -in client.csr -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out client-cert.pem -days 3650
+openssl x509 -req -in client.csr -CA ca-cert.pem -CAkey ca-key.pem \
+-CAcreateserial -out client-cert.pem -days 3650
