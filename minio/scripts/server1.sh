668ae6d8aeb88ec082047c660792970e2a82ba3b
chiefexb <chief_exb@mail.ru>
2025-04-23 16:58:19 +0300

    123

--- minio/scripts/server1.sh
+++ minio/scripts/server1.sh
@@ -1,15 +1,22 @@
 # Добавьте удаленный целевой бакет на server1
-server2=185.43.7.214
-server1=195.140.146.176
-mc admin bucket remote add $server1/test \
-  http://$server2:9000/test \
-  --service "replication" \
+mc alias set server1 http://195.140.146.176:9000 minio minio123
+mc alias set server2 http://185.43.7.214:9000 minio minio
+
+
+# Алиасы серверов (должны быть созданы через mc alias заранее!)
+SERVER1_ALIAS="server1"
+SERVER2_ALIAS="server2"
+
+# Добавление удаленного бакета
+mc admin bucket remote add $SERVER1_ALIAS/test \
+  http://$SERVER2_ALIAS:9000/test \
+  --service-type "replication" \
   --region "us-east-1" \
   --access-key minio \
   --secret-key minio123
 
-# Создайте правило репликации
-mc replicate add $server1/test \
-  --remote-bucket http://$server2:9000/test \
+# Создание правила репликации
+mc replicate add $SERVER1_ALIAS/test \
+  --remote-bucket $SERVER2_ALIAS/test \
   --replicate "delete,delete-marker,existing-objects" \
   --priority 1
--- minio/scripts/server2.sh
+++ minio/scripts/server2.sh
@@ -1,15 +1,22 @@
 # Добавьте удаленный целевой бакет на server1
-server2=185.43.7.214
-server1=195.140.146.176
-mc admin bucket remote add $server2/test \
-  http://$server1:9000/test \
-  --service "replication" \
+mc alias set server1 http://195.140.146.176:9000 minio minio123
+mc alias set server2 http://185.43.7.214:9000 minio minio
+
+
+# Алиасы серверов (должны быть созданы через mc alias заранее!)
+SERVER1_ALIAS="server1"
+SERVER2_ALIAS="server2"
+
+# Добавление удаленного бакета
+mc admin bucket remote add $SERVER2_ALIAS/test \
+  $SERVER1_ALIAS/test \
+  --service-type "replication" \
   --region "us-east-1" \
   --access-key minio \
   --secret-key minio123
 
-# Создайте правило репликации
-mc replicate add $server2/test \
-  --remote-bucket http://$server1:9000/test \
+# Создание правила репликации
+mc replicate add $SERVER2_ALIAS/test \
+  --remote-bucket $SERVER1_ALIAS/test \
   --replicate "delete,delete-marker,existing-objects" \
   --priority 1
