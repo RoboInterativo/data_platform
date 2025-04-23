19275b030cbd2e81ebe1b0cd8492222e9ee6d6cd
chiefexb <chief_exb@mail.ru>
2025-04-23 17:25:28 +0300

    123

--- minio/scripts/server1.sh
+++ minio/scripts/server1.sh
@@ -1,6 +1,6 @@
 # Добавьте удаленный целевой бакет на server1
-mc alias set server1 http://195.140.146.176:9000 minio minio123
-mc alias set server2 http://185.43.7.214:9000 minio minio
+# mc alias set server1 http://195.140.146.176:9000 minio minio123
+# mc alias set server2 http://185.43.7.214:9000 minio minio
 
 
 # Алиасы серверов (должны быть созданы через mc alias заранее!)
@@ -9,7 +9,7 @@ SERVER2_ALIAS="server2"
 
 # Добавление удаленного бакета
 mc admin bucket remote add $SERVER1_ALIAS/test \
-  http://$SERVER2_ALIAS:9000/test \
+  http://$SERVER2_ALIAS/test \
   --service-type "replication" \
   --region "us-east-1" \
   --access-key minio \
