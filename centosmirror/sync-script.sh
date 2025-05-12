dbac1a0e6d4042a10548674932b2e152d9f7c5f2
chiefexb <chief_exb@mail.ru>
2025-05-13 01:00:00 +0300

    123

--- centosmirror/sync-script.sh
+++ centosmirror/sync-script.sh
@@ -5,16 +5,28 @@ BASE="https://mirror.yandex.ru/centos-stream"
 REPO_BASEOS="$BASE/9-stream/BaseOS/x86_64/os/"
 REPO_APPSTREAM="$BASE/9-stream/AppStream/x86_64/os/"
 
+# # Синхронизация BaseOS
+# rsync -avz --delete --exclude='repodata' -v \
+#   rsync://mirror.centos.org/centos/9-stream/BaseOS/x86_64/os/ \
+#   /mirror/baseos
+#
+# # Синхронизация AppStream
+# rsync -avz --delete --exclude='repodata' -v \
+#   rsync://mirror.centos.org/centos/9-stream/AppStream/x86_64/os/ \
+#   /mirror/appstream
 # Синхронизация BaseOS
-rsync -avz --delete --exclude='repodata' -v \
-  rsync://mirror.centos.org/centos/9-stream/BaseOS/x86_64/os/ \
-  /mirror/baseos
+reposync \
+  --repo=baseos \
+  --download-metadata \
+  --newest-only \
+  --download-path=/mirror/baseos
 
 # Синхронизация AppStream
-rsync -avz --delete --exclude='repodata' -v \
-  rsync://mirror.centos.org/centos/9-stream/AppStream/x86_64/os/ \
-  /mirror/appstream
-
+reposync \
+  --repo=appstream \
+  --download-metadata \
+  --newest-only \
+  --download-path=/mirror/appstream
 # Обновление метаданных
 createrepo_c --update /mirror/baseos
 createrepo_c --update /mirror/appstream
